1fb3f9f4c1173a6c3d61067cbb71e9ec4e289834
piyushsolanki1 <solankipiyush6262@gmail.com>
2025-10-25 16:40:27 +0530

    added features

--- src/components/TrendingAnime.jsx
+++ src/components/TrendingAnime.jsx
@@ -1,22 +1,36 @@
-import React from 'react'
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
+const TrendingAnime = ({ trendingAnimes = [], loading }) => {
+  const navigate = useNavigate();
 
+  if (loading) return <p className="text-center">Loading trending animes...</p>;
 
-const TrendingAnime = ({ trendingAnimes= [], loading }) => {
-    if (loading) return <p className="text-center">Loading trending animes...</p>;
-
-    return (
-        <div className="trending-animes">
-            <h2 className='text-left pl-10 pb-15'>Trending Animes</h2>
-            <ul className='pl-20'>
-                {trendingAnimes.map((anime, index) => (
-                    <li key={anime.mal_id}>
-                        <img className='h-full' src={anime.images.jpg.image_url} alt="" />
-                    </li>
-                ))}
-            </ul>
+  return (
+    <div className="trending-animes">
+      <h2 className="text-left pl-10 pb-15">Trending Animes</h2>
+      <div>
+      <ul className="pl-20 flex gap-4 overflow-x-auto">
+        <div className="flex contain-content gap-8 ">
+        {trendingAnimes.map((anime) => (
+          <li key={anime.mal_id} className="cursor-pointer flex flex-col items-center shadow w-full">
+            
+            <img
+              className="h-full w-full"
+              src={anime.images?.jpg?.image_url || anime.images?.webp?.image_url}
+              alt={anime.title}
+              onClick={() => navigate(`/watch/anime/${anime.mal_id}`)}
+            />
+            <p className="text-white text-sm text-left ">{anime.title}</p>
+            
+          </li>
+        ))}
         </div>
-    )
-}
+      </ul>
+      </div>
+    </div>
+  );
+};
 
-export default TrendingAnime
+export default TrendingAnime;
+
--- src/components/TrendingManga.jsx
+++ src/components/TrendingManga.jsx
@@ -1,19 +1,33 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const TrendingManga = ({ mangas = [], loading }) => {
+  const navigate = useNavigate();
+
   if (loading) return <p className="text-center">Loading trending mangas...</p>;
 
   return (
     <div className="trending-animes">
       <h2 className="text-left pl-10 pb-15">Trending Manga</h2>
-      <ul className="pl-20">
+      <ul className="pl-20 flex gap-4 overflow-x-auto">
+      <div className="flex contain-content gap-8 ">
         {mangas.map((manga, index) => (
-          <li key={manga.mal_id}>
-            <img className="h-full" src={manga.images.jpg.image_url} alt={manga.title} />
+          <li key={manga.mal_id} className="cursor-pointer flex flex-col items-center shadow w-full">
+            <img
+              className="h-full w-full"
+              src={
+                manga.images?.jpg?.image_url || anime.images?.webp?.image_url
+              }
+              alt={manga.title}
+              onClick={() => navigate(`/watch/manga/${manga.mal_id}`)}
+            />
+            <p className="text-white text-sm text-left">{manga.title}</p>
           </li>
+          
         ))}
+        </div>
       </ul>
-    </div>
+      </div>
   );
 };
 
